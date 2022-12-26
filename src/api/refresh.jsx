d28b675723787a060dabd01fc1b2db7f56e6f212
Yusufzoda Dovudbek <80512749+woodo8@users.noreply.github.com>
2022-12-27 00:34:28 +0300

    Update refresh.jsx

--- src/api/refresh.jsx
+++ src/api/refresh.jsx
@@ -1,17 +1,1 @@
-import { Navigate, useNavigate } from "react-router-dom"
 
-export default function refresh(code, statusText) {
-    // const navigate = useNavigate()
-
-    if (code == 401 || code == 403 || statusText === "Unauthorized") {
-        localStorage.removeItem("access")
-        // navigate("/")
-    }
-}
-// import React from 'react'
-
-// export default function refresh() {
-//     return (
-//         <div>refresh</div>
-//     )
-// }
