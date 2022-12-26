8556d0d1959240541d6d25c9d8751c22892ea5a3
Yusufzoda Dovudbek <80512749+woodo8@users.noreply.github.com>
2022-12-27 00:33:46 +0300

    Update protectedRoutes.jsx

--- src/routes/protectedRoutes.jsx
+++ src/routes/protectedRoutes.jsx
@@ -1,9 +1,1 @@
-import React, { useContext } from "react";
-import { Navigate, Outlet } from "react-router-dom";
-import { StateContext } from "../context/context";
 
-const ProtectedRoutes = () => {
-    const loggedIn = localStorage.getItem("access")
-    return loggedIn ? <Outlet /> : <Navigate to="/" />;
-};
-export default ProtectedRoutes;
